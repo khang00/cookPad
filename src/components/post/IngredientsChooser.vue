70631d768736038715872a622d28d0b7152051a9
= <=>
2020-04-02 14:37:46 +0700

    UI for adding main and sub-ingredients

---
+++ src/components/post/IngredientsChooser.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-combobox
+    v-model="ingredients"
+    :items="existedIngredients"
+    color="yellow darken-3"
+    label="Your main ingredients"
+    prepend-icon="fas fa-carrot"
+    chips
+    multiple
+    outlined
+  >
+    <template v-slot:selection="{ attrs, item, select, selected }">
+      <v-chip
+        v-bind="attrs"
+        :input-value="selected"
+        color="yellow darken-3"
+        class="white--text"
+        close
+        @click="select"
+        @click:close="remove(item)"
+      >
+        <strong>{{ item }}</strong>
+      </v-chip>
+    </template>
+  </v-combobox>
+</template>
+
+<script>
+export default {
+  name: 'IngredientsChooser',
+  props: {
+    ingredients: {
+      type: Array,
+      default: null
+    }
+  },
+  computed: {
+    existedIngredients() {
+      const ingredients = ['meat', 'veggies', 'salt', 'sugar']
+      return ingredients
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/post/PostContentUpload.vue
+++ src/components/post/PostContentUpload.vue
@@ -11,6 +11,15 @@
           prepend-icon="fas fa-closed-captioning"
           outlined
         ></v-text-field>
+        <ingredient-chooser v-model="mainIngredients" />
+        <v-textarea
+          v-model="post.subIngredients"
+          class="mt-5"
+          color="yellow darken-3"
+          label="Additional Ingredients"
+          prepend-icon="fas fa-carrot"
+          outlined
+        ></v-textarea>
       </v-container>
     </v-card-actions>
   </v-card>
@@ -18,17 +27,21 @@
 
 <script>
 import PostImagesUpload from '@/components/post/PostImagesUpload.vue'
+import IngredientsChooser from '@/components/post/IngredientsChooser.vue'
 
 export default {
   name: 'PostContentUpload',
   components: {
-    'post-images-upload': PostImagesUpload
+    'post-images-upload': PostImagesUpload,
+    'ingredient-chooser': IngredientsChooser
   },
   data() {
     return {
       post: {
         caption: '',
-        images: []
+        images: [],
+        mainIngredients: [],
+        subIngredients: ''
       }
     }
   }
